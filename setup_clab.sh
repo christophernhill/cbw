81d6bb4f645501d0b489fe82fd6d85e96a06f9d2
Chris Hill <cnh@mit.edu>
2023-06-22 23:10:28 -0400

    Update setup_clab.sh

--- setup_clab.sh
+++ setup_clab.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 curl -O https://svante.mit.edu/'~cnh/d7/ddd.tgz'
 tar -xzvf ddd.tgz
+(mkdir -p foo; cd foo; tar -xvf ../mitgcm_and_dar7.tar)
+(cd foo/dar7/build_1D; \rm -fr *; ../../tools/genmake2 -mods=../code_6+4+0_RT_1D)
+(cd foo/dar7/build_1D; make)
+(cd foo/dar7; mkdir -p run)
+(cd foo/dar7/run; ln -s ../input_6+4+0_RT_1D/* .; ln -s ../build_1D/mitgcmuv .)
+(cd foo/dar7/run; ./mitgcmuv )
